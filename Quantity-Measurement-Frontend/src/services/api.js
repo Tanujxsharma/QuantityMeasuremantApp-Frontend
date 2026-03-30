b29061f3d4e9a0927fd020cfe9024e5522968ef3
Tanuj Sharma <ta904513@gmail.com>
2026-03-30 20:19:42 +0530

    [tanuj sharma ]Refactored

--- Quantity-Measurement-Frontend/src/services/api.js
+++ Quantity-Measurement-Frontend/src/services/api.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL 
-  || "https://quantitymeasurementapp-production-1dc5.up.railway.app";
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://quantitymeasurementapp-production-1dc5.up.railway.app";
 
+const API = axios.create({
+  baseURL: BASE_URL,
+});
+
+// interceptor
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
