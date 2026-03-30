f65713d0c0fe3271248c23274386379c74883e0f
Tanuj Sharma <ta904513@gmail.com>
2026-03-30 19:30:19 +0530

    [Tanuj Sharma ]add . env

--- Quantity-Measurement-Frontend/src/services/api.js
+++ Quantity-Measurement-Frontend/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:8080"
-});
+const API =  import.meta.env.VITE_API_URL;
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
